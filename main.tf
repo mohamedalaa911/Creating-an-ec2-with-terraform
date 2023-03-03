7398571370b37a37e22c2124e908f8cddffcd6ec
mohamedalaa911 <mohamed_alaa1097@yahoo.com>
2023-03-03 14:52:30 +0200

    first commit

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "my-ec2"{
     ami="ami-0f3c9c466bb525749"
     instance_type="t2.micro"
     tags = {
-        Name = "moalaa-ec2-instance"
+        Name = "mo-ec2-instance"
     }
 }
+resource  "aws_eip" "my-eip"{
+    vpc = true
+}
+resource "aws_eip_association" "associate"{
+    instance_id=aws_instance.my-ec2.id
+    allocation_id=aws_eip.my-eip.id
+
+}
+
